8928a846711ce38575432ba2eb978c8d4d738198
Ratul Ganguly <rgratulganguly1234@outlook.com>
2022-08-14 10:44:05 +0530

    Added property source for local properties file

--- src/main/java/com/myschool/timetable/TimeTableApiApplication.java
+++ src/main/java/com/myschool/timetable/TimeTableApiApplication.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@PropertySource("classpath:application-local.properties")
 @EnableSwagger2
 @OpenAPIDefinition(
         info = @Info(
