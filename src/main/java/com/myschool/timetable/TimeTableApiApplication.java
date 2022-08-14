1a788ef2601ccc0ac23540cb80ce0719665b3692
Ratul Gangopadhyay <111196705+ratul-gangopadhyay@users.noreply.github.com>
2022-08-14 10:50:27 +0530

    Added @PropertySource (#3)
    * Added property source for local properties file
    
    * Added property source for local properties file

--- src/main/java/com/myschool/timetable/TimeTableApiApplication.java
+++ src/main/java/com/myschool/timetable/TimeTableApiApplication.java
@@ -4,14 +4,16 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@PropertySource("classpath:application-local.properties")
 @EnableSwagger2
 @OpenAPIDefinition(
         info = @Info(
                 title = "time-table-api",
-                description = "A collection of apis for creating a non clashing Time table"
+                description = "A collection of apis for generating a non clashing Time table"
         ))
 public class TimeTableApiApplication {
 
